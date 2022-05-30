2612dbc05245cf68fb7dac600c9c887f308da1d7
Nitin <nitin.rajput20@gmail.com>
2022-05-30 19:21:30 +0530

    First Commit for Maven Spring Boot Docker POC

---
+++ src/main/java/com/example/springbootdocker/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.springbootdocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String getDemoString() {
+        return "Demo";
+    }
+}
